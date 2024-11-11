0eef5a882bce0886b8664756e5e195efaebbb0ec
Pawan Kumar MTTS <pawan.kumar@mt.com>
2024-11-11 18:48:38 +0530

    delete and update

--- Grpc.Test/ConferenceTest.cs
+++ Grpc.Test/ConferenceTest.cs
@@ -8,8 +8,9 @@ namespace Grpc.Test
     [TestClass]
     public class ConferenceTest
     {
-        private ConferenceClient? conferenceClient;
         private readonly string TopicId = "7229";
+        private ConferenceClient? conferenceClient;
+        private static readonly Dictionary<string, string>? storage = [];
 
         [TestInitialize]
         public void Start()
@@ -42,14 +43,17 @@ namespace Grpc.Test
             var createResponse = await conferenceClient!.CreateSpeakerDetailsAsync(request);
             createResponse.Should().NotBeNull();
             createResponse.Should().BeEquivalentTo(expectedResponse);
+            storage?.TryAdd(TopicId, request.TopicId);
         }
 
         [TestMethod]
         public async Task GetConferenceDetailByTopicId()
         {
+            string? storedTopicId = GetConferenceTopicId();
+
             GetConferenceRequest requestGet = new()
             {
-                TopicId = TopicId
+                TopicId = storedTopicId
             };
 
             // response to get new conference entry by using topicId
@@ -61,9 +65,11 @@ namespace Grpc.Test
         [TestMethod]
         public async Task UpdateConferenceDetails()
         {
+            string? storedTopicId = TopicId;
+
             var requestUpdate = new ConferenceData
             {
-                TopicId = TopicId,
+                TopicId = storedTopicId,
                 Title = $"GRPC_Demo_update {RandomNumber.Next(60, 100)}",
                 Author = Name.FullName(),
                 Coauthor = $"Updated Name {Name.First}",
@@ -71,7 +77,67 @@ namespace Grpc.Test
                 Duration = RandomNumber.Next(60, 100)
             };
 
+            var expectedResponse = new UpdateConferenceResponse
+            {
+                Success = "Updated",
+                TopicId = storedTopicId,
+            };
+
+            // response to update conference entry
             var updateResponse = await conferenceClient!.UpdateSpeakerDetailsAsync(requestUpdate);
+            updateResponse.Should().NotBeNull();
+            updateResponse.Should().BeEquivalentTo(expectedResponse);
+        }
+
+        [TestMethod]
+        public async Task UpdateNonExistingConferenceDetails()
+        {
+            const string nonExistingTopicId = "123456";
+            var requestUpdate = new ConferenceData
+            {
+                TopicId = nonExistingTopicId,
+                Title = $"GRPC_Demo_update {RandomNumber.Next(60, 100)}"
+            };
+
+            var expectedResponse = new UpdateConferenceResponse
+            {
+                Success = "Failure, No existing topic id found to update",
+                TopicId = nonExistingTopicId,
+            };
+
+            // response to update non existing conference entry
+            var updateResponse = await conferenceClient!.UpdateSpeakerDetailsAsync(requestUpdate);
+            updateResponse.Should().NotBeNull();
+            updateResponse.Should().BeEquivalentTo(expectedResponse);
+        }
+
+        [TestMethod]
+        public async Task DeleteConferenceDetails()
+        {
+            string? storedTopicId = GetConferenceTopicId();
+            DeleteConferenceRequest requestDelete = new()
+            {
+                TopicId = storedTopicId
+            };
+
+            var expectedResponse = new DeleteConferenceResponse
+            {
+                Success = "Deleted",
+                TopicId = storedTopicId,
+            };
+
+            // response to delete conference entry
+            var updateResponse = await conferenceClient!.DeleteSpeakerDetailsAsync(requestDelete);
+            updateResponse.Should().NotBeNull();
+            updateResponse.Should().BeEquivalentTo(expectedResponse);
+        }
+
+        private string GetConferenceTopicId()
+        {
+            var storedTopicId = string.Empty;
+            storage?.TryGetValue(TopicId, out storedTopicId);
+            storedTopicId ??= TopicId;
+            return storedTopicId;
         }
     }
 }
